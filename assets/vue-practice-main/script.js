a6829d84cbb62713ac06ea2aabebfeecb47cd314
Artin0123 <101245159+Artin0123@users.noreply.github.com>
2022-06-15 05:54:27 +0000

    1

---
+++ assets/vue-practice-main/script.js
@@ -0,0 +1,21 @@
+let pageData = {
+    members: [
+        {
+            name: 'Susan',
+            gender: 'Female',
+        },
+        {
+            name: 'Peter',
+            gender: 'Male',
+        },
+        {
+            name: 'Bill',
+            gender: 'Unknow',
+        }],
+};
+const app = Vue.createApp({
+    data() {
+        return pageData;
+    }
+});
+app.mount("#app");
